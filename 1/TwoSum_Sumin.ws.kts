e7818c9135026bcf1982c09925c8ea45a4630310
SuminGo <jennifer045020@gmail.com>
2022-10-25 21:10:25 +0900

    twoSum solved

---
+++ 1/TwoSum_Sumin.ws.kts
@@ -0,0 +1,14 @@
+fun main() {
+    twoSum(intArrayOf(2, 7, 11, 15), 9)
+}
+
+fun twoSum(nums: IntArray, target: Int): IntArray {
+    for (i in nums.indices) {
+        for (j in i + 1 until nums.size) {
+            if (nums[i] + nums[j] == target) {
+                return intArrayOf(i, j)
+            }
+        }
+    }
+    return intArrayOf()
+}
