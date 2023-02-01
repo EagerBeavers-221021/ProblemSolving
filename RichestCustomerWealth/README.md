3dec36be1a2effb3684dc4a76fa4933caec1d707
SuminGo <jennifer045020@gmail.com>
2023-02-01 17:16:57 +0900

    RichestCustomerWealth

---
+++ RichestCustomerWealth/README.md
@@ -0,0 +1,5 @@
+# Two Sum
+https://leetcode.com/problems/richest-customer-wealth/submissions/889278360/
+난이도 : 하
+
+주어진 이중 배열에서 각 배열의 합 중 가장 큰 값을 찾는 문제
---
+++ RichestCustomerWealth/RunningSum.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun maximumWealth(accounts: Array<IntArray>): Int {
+        var max = 0
+        accounts.forEach {
+            val sum = it.sum()
+            if(sum > max) {
+                max = sum
+            }
+        }
+        return max
+    }
+}
